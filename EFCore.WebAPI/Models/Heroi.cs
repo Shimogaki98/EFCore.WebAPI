71126d50773a3f08029a25cd41d0484cffc1ef5d
Marcello Bronzatti <mbsimogaki@gmail.com>
2020-11-16 10:53:30 -0300

    Add project files.

---
+++ EFCore.WebAPI/Models/Heroi.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace EFCore.WebAPI.Models
+{
+    public class Heroi
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public IdentidadeSecreta IdentidadeSecreta { get; set; }
+        public List<Arma> Armas { get; set; }
+        public List<HeroiBatalha> HeroisBatalhas { get; set; }
+    }
+}
